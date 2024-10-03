6b776ce56dbff480eb437f4cf2cbc480dd2f6fd1
Renzo Damian <renzo.damian.go@gmail.com>
2024-10-03 15:45:15 +0100

    delete javadoc files

--- Lab01/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"<Unnamed>","c":"QuadraticSolver","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"<Unnamed>","c":"QuadraticSolver","l":"QuadraticSolver()","u":"%3Cinit%3E()"}];updateSearchResults();
