7b7e6eada070bdb163608ad44cee20cb21d2e963
Renzo Damian <renzo.damian.go@gmail.com>
2024-10-03 16:26:23 +0100

    Finish excercise 2

--- Lab01/QuadraticSolver.java
+++ Lab01/QuadraticSolver.java
@@ -4,7 +4,7 @@
  * Copyright (c) University of Sheffield 2011
  */
 
-import java.math.*;
+import sheffield.EasyReader;
 
 /**
 * QuadraticSolver.java
@@ -18,25 +18,34 @@ import java.math.*;
 */
 public class QuadraticSolver {
     public static void main( String[] arg) {
+			EasyReader keyboard = new EasyReader();
+			double a = keyboard.readDouble("Input a value for a: ");
+			double b = keyboard.readDouble("Input a value for b: ");
+			double c = keyboard.readDouble("Input a value for c: ");
 
-	// default values for coefficients a, b, and c
-	// initially, these are stored as both integers and float
-	int aInt = 1, bInt = 2, cInt = 1;
-	double aFloat = 1, bFloat = 2, cFloat = 1;
+			// default values for coefficients a, b, and c
+			// initially, these are stored as both integers and float
+			// int aInt = 1, bInt = 2000000, cInt = 1;
+			// double aFloat = 1, bFloat = 2000000, cFloat = 1;
 
-	// declare variables to store the two values of x that satisfy the equation
-	double x1, x2;
+			// declare variables to store the two values of x that satisfy the equation
+			double x1, x2;
 
-	// work out the solution with int types
-	aInt -= 1/2;
-	x1 = (-1 * bInt + Math.sqrt(bInt*bInt - 4 * aInt * cInt)) / (2 * aInt);
-	x2 = (-1 * bInt - Math.sqrt(bInt*bInt - 4 * aInt * cInt)) / (2 * aInt);
-	System.out.println("Solution with integer types is x1 = " + x1 + ", and x2 = " + x2);
+			// work out the solution with int types
+			// x1 = (-1 * bInt + Math.sqrt(bInt*bInt - 4 * aInt * cInt)) / (2 * aInt);
+			// x2 = (-1 * bInt - Math.sqrt(bInt*bInt - 4 * aInt * cInt)) / (2 * aInt);
+			// System.out.println("Solution with integer types is x1 = " + x1 + ", and x2 = " + x2);
 
-	// work out the solution with double types
-	aFloat -= 0.5;
-	x1 = (-1 * bFloat + Math.sqrt(bFloat*bFloat - 4 * aFloat * cFloat)) / (2 * aFloat);
-	x2 = (-1 * bFloat - Math.sqrt(bFloat*bFloat - 4 * aFloat * cFloat)) / (2 * aFloat);
-	System.out.println("Solution with double types is  x1 = " + x1 + ", and x2 = " + x2);
+			// work out the solution with double types
+			x1 = (-1.0 * b + Math.sqrt(b*b - 4.0 * a * c)) / (2.0 * a);
+			x2 = (-1.0 * b - Math.sqrt(b*b - 4.0 * a * c)) / (2.0 * a);
+			System.out.println("Solution with double types is  x1 = " + x1 + ", and x2 = " + x2);
+			double sol1 = a* Math.pow(x1,2.0)+b*x1+ c;
+			double sol2 = a* Math.pow(x2,2.0)+b*x2 + c;
+
+			System.out.printf("%.5f\n",sol1);
+			System.out.printf("%.5f\n",sol2);
+
+
     }
 }
