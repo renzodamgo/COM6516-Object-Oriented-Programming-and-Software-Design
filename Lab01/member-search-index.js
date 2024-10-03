f44b1c5a7c2cb788fe45a1010ee975662ad35dde
Renzo Damian <renzo.damian.go@gmail.com>
2024-10-03 15:42:48 +0100

    add file

---
+++ Lab01/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"QuadraticSolver","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"<Unnamed>","c":"QuadraticSolver","l":"QuadraticSolver()","u":"%3Cinit%3E()"}];updateSearchResults();
