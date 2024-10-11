e578570fed01668fb9b0554c2a4cad5c6d82f38e
Renzo Damian <renzo.damian.go@gmail.com>
2024-10-11 11:20:43 +0100

    Optimeze classes

--- Lab02/Basket.java
+++ Lab02/Basket.java
@@ -1,24 +1,24 @@
-/** Basket.java
-*
-* Part of lab class for COM6516
-* Written by Mark Stevenson mark.stevenson@sheffield.ac.uk
-* Based on code written by Steve Maddock
-* Last modified 19 September 2014
-*
-*/
 
+/** Basket.java
+ *
+ * Part of lab class for COM6516
+ * Written by Mark Stevenson mark.stevenson@sheffield.ac.uk
+ * Based on code written by Steve Maddock
+ * Last modified 19 September 2014
+ *
+ */
 public class Basket {
 
     public Basket(Item[] it) {
-	items = it;
+        items = it;
     }
 
     public double total() {
-	double tot = 0.0;
-	for (int i = 0; i < items.length; i++) {
-	    tot += items[i].getPrice();
-	}
-	return tot;
+        double tot = 0.0;
+        for (Item item : items) {
+            tot += item.getPrice();
+        }
+        return tot;
     }
 
     private Item[] items;
