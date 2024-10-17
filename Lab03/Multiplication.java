fccf15690faab9327e5f51d606bb143598d8a175
Renzo Damian <renzo.damian.go@gmail.com>
2024-10-17 15:42:03 +0100

    Add better format

--- Lab03/Multiplication.java
+++ Lab03/Multiplication.java
@@ -1,14 +1,28 @@
 
 public class Multiplication {
   public static void main(String[] args) {
-    for (int i = 1; i <= 9; i++) {
-      for (int j = 1; j <= 9; j++) {
-        int n = i * j;
-        if (n > 9) {
-          System.out.printf("%d\t", i * j);
+    for (int k = 0; k <= 9; k++) {
+      if (k == 0) {
+        System.out.printf("    ", k);
 
+      } else {
+        System.out.printf(" %d  ", k);
+      }
+    }
+    System.out.printf("\n----------------------------------------\n");
+
+    for (int i = 1; i <= 9; i++) {
+      for (int j = 0; j <= 9; j++) {
+        int n = i * j;
+        if (n == 0) {
+          System.out.printf("%d | ", i);
         } else {
-          System.out.printf(" %d\t", i * j);
+          if (n > 9) {
+            System.out.printf("%d  ", n);
+
+          } else {
+            System.out.printf(" %d  ", n);
+          }
         }
 
       }
